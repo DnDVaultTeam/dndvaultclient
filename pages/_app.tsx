4afe5969ce2d0e104c98028b0c74efa70e19d001
maxim <maximmukhanov.job@gmail.com>
2022-07-06 17:41:23 +0000

    ci: implements base structure

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
+import { NextPageWithLayout } from './page';
+
+interface AppPropsWithLayout extends AppProps {
+  Component: NextPageWithLayout;
+}
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout || ((page) => page);
+  return getLayout(<Component {...pageProps} />);
+}
+
+export default MyApp;
